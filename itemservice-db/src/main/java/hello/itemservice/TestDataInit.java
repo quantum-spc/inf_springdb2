ced7c8889ceec079ca8710f253c1ade0262bc3b1
qtm <zmxnxnxn1@naver.com>
2022-12-08 18:11:46 +0900

    애플리케이션 중지 테스트 코드 추가

--- itemservice-db/src/main/java/hello/itemservice/TestDataInit.java
+++ itemservice-db/src/main/java/hello/itemservice/TestDataInit.java
@@ -5,6 +5,7 @@ import hello.itemservice.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.ContextClosedEvent;
 import org.springframework.context.event.EventListener;
 
 @Slf4j
@@ -21,6 +22,14 @@ public class TestDataInit {
         log.info("test data init");
         itemRepository.save(new Item("itemA", 10000, 10));
         itemRepository.save(new Item("itemB", 20000, 20));
+    }
+
+    /**
+     * 애플리케이션 중지 이벤트
+     */
+    @EventListener(ContextClosedEvent.class)
+    public void onContextClosedEvent(ContextClosedEvent event) {
+        System.out.println(" 프로젝트 종료 @@@ ContextClosedEvent @@@");
     }
 
 }
